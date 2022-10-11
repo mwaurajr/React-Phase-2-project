b4a60b30aff0baf4488d0a7b1969312a3590a7f7
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-10-11 23:15:55 +0300

    finished feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import restProvider from "ra-data-simple-rest";
+import PostList from "./Components/PostList";
+import PostCreate from "./Components/PostCreate";
+import PostEdit from "./Components/PostEdit";
+import UserList from "./Components/UserList";
+import UserCreate from "./Components/UserCreate";
+import UserEdit from "./Components/UserEdit";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin dashboard={Dashboard} dataProvider={restProvider("http://localhost:3000")} >
+      <Resource
+        name="posts"
+        list={PostList}
+        create={PostCreate}
+        edit={PostEdit}
+      />
+      <Resource
+        name="users"
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
+      />
+      
+    </Admin>
+    
   );
 }
 
